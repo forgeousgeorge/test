6f1be2e6e8f80ffb83396e8da634caffde189b46
Jan-Torge Schindler <jtschindler@email.arizona.edu>
2017-04-24 15:42:37 -0600

    Completed first Function

---
+++ code.py
@@ -0,0 +1,6 @@
+
+import numpy as np
+
+def calc_circ_area(radius):
+    A = np.pi*radius**2
+    print(A)# this will make it much easier in future problems to see that something is actually happening
