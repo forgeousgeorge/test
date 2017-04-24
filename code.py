b062bf64c575709fedbcacf33cc1cbcf0b73936e
George Vejar <forgeousgeorge@gmail.com>
2017-04-24 15:27:02 -0700

    added square area

--- code.py
+++ code.py
@@ -3,4 +3,8 @@ import numpy as np
 
 def calc_circ_area(radius):
     A = np.pi*radius**2
-    print(A)# this will make it much easier in future problems to see that something is actually happening
+    print A # this will make it much easier in future problems to see that something is actually happening
+
+def calc_square_area(length, width):
+    A = length*width
+    print A
